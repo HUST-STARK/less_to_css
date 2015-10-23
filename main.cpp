e04465b948aa32c881e39e811d26731494982cc0
zhangji <macinchang@gmail.com>
2015-10-24 00:23:03 +0800

    Signed-off-by: zhangji <macinchang@gmail.com>

--- main.cpp
+++ main.cpp
@@ -11,11 +11,76 @@
 
 using namespace std;
 
+
+struct Comment{
+    vector<string> value;
+    Comment(){
+        this->value.clear();
+    }
+};
+struct Var{
+    bool is_stable;
+    int type;
+    vector<Var*> son;
+    int _int;
+    float _float;
+    string _string;
+    int comment_var;
+    Var(){};
+};
+
+struct Property{
+    string name;
+    int comment_name;
+    Var *value;
+    Property(string name = "", int comment_name = -1, Var *value = NULL):name(name), comment_name(comment_name), value(value){};
+};
+
+struct Class{
+    string title;
+    int comment_title;
+    vector<Property> value;
+    int comment_class;
+};
+
+struct Word{
+    int type;
+    string value;
+};
+//输入数据缓冲
 vector<char> buf;
+//单词表
+vector<Word> words;
+//注释表
+vector<Comment> comments;
+
 void read(){
 //将文件中所有字符读到buf中
 }
+
+void get_words(){
+//将单词读入到words中
+};
+
+void turn(){
+//将单词处理成变量储存
+    int pos = 0, len = words.size();
+    while(pos < len){
+        int type = words[pos++].type;
+        if(type == "标签"){
+
+        }
+    }
+}
 int main(){
     freopen("in.txt", "r", stdin);
+    //将less读入到buf中
     read();
+    //将每个单词读入到words中
+    get_words();
+
+    turn();
+
+
+
 }
