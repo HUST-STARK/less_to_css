f18025f07d58002eca7b109b6df4af86e59f29f1
zhangji <macinchang@gmail.com>
2015-10-24 17:40:52 +0800

    Signed-off-by: zhangji <macinchang@gmail.com>

--- main.cpp
+++ main.cpp
@@ -9,6 +9,20 @@
 #define STRING      3
 #define OPERATOR    4
 
+//word中type的定义
+#define WORD_VAR    0   //变量
+#define WORD_COLON  1   //冒号
+#define WORD_NUM    2   //数字
+#define WORD_TITLE  3   //函数名或类名或属性名或颜色常量
+#define WORD_OB     4   //左大括号 opening brace
+#define WORD_CB     5   //右大括号 closing brace
+#define WORD_STR    6   //字符串(待处理,中间可能还有变量)
+#define WORD_EQU    7   //等于号
+#define WORD_COMMA  8   //逗号
+#define WORD_OP     9   //左小括号
+#define WORD_CP     10  //右小括号
+#define WORD_SEM    11  //分号
+
 using namespace std;
 
 
@@ -24,6 +38,7 @@ struct Var{
     vector<Var*> son;
     int _int;
     float _float;
+    string _color;
     string _string;
     int comment_var;
     Var(){};
@@ -47,6 +62,14 @@ struct Word{
     int type;
     string value;
 };
+//单词表中的位置
+struct Pos{
+    int pos_begin, pos_end;
+};
+//存函数在单词表中的位置
+vector<Pos> pos_of_func;
+
+map<string, int> map_func;
 //输入数据缓冲
 vector<char> buf;
 //单词表
@@ -56,10 +79,12 @@ vector<Comment> comments;
 
 void read(){
 //将文件中所有字符读到buf中
+
 }
 
 void get_words(){
 //将单词读入到words中
+
 };
 
 void turn(){
@@ -67,7 +92,10 @@ void turn(){
     int pos = 0, len = words.size();
     while(pos < len){
         int type = words[pos++].type;
-        if(type == "标签"){
+        //碰到变量
+        if(type == WORD_VAR){
+                deal_var(pos);
+        } else if(type == WORD_TITLE){
 
         }
     }
@@ -80,7 +108,5 @@ int main(){
     get_words();
 
     turn();
-
-
 
 }
